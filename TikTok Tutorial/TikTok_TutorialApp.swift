c5a69209b1e35c5b25091d784f4bc9de95133d06
Oncu <oohancan@gmail.com>
2022-05-05 20:18:21 +0300

    Initial Commit

---
+++ TikTok Tutorial/TikTok_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_TutorialApp.swift
+//  TikTok Tutorial
+//
+//  Created by Oncu Ohancan on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
