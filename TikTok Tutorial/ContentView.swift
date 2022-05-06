40c48881e72f659f1aa9b1c5cb006d10ceaaf1fe
Oncu <oohancan@gmail.com>
2022-05-06 21:52:48 +0300

    add button

--- TikTok Tutorial/ContentView.swift
+++ TikTok Tutorial/ContentView.swift
@@ -22,13 +22,74 @@ struct ContentView_Previews: PreviewProvider {
 struct Home: View {
     
     @State var index = 0
+    @State var top = 0
     
     var body: some View {
         ZStack{
             VStack{
+//                Top Menu
+                HStack(spacing: 15) {
+                    Button{self.top = 0} label: {
+                        Text("Following")
+                            .foregroundColor(self.top == 0 ? .white : .white.opacity(0.45))
+                            .fontWeight(self.top == 0 ? .bold : .none)
+                            .padding(.vertical)
+                       }
+                           Button {self.top = 1} label: {
+                        Text("For You")
+                            .foregroundColor(self.top == 1 ? .white : .white.opacity(0.45))
+                            .fontWeight(self.top == 1 ? .bold : .none)
+                            .padding(.vertical)
+                    }
+                }
                 Spacer()
+//                Right Menu
+                HStack {
+                    Spacer()
+                    VStack(spacing: 35) {
+                        Button(action: {}, label: {
+                            Image("pic")
+                                .renderingMode(.original)
+                                .resizable()
+                                .frame(width: 55, height: 55)
+                                .clipShape(Circle())
+                        })
+                        Button(action: {}, label: {
+                            VStack {
+                            Image(systemName: "suit.heart.fill")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                Text("400K")
+                                    .foregroundColor(.white)
+                            }
+                        })
+                        Button(action: {}, label: {
+                            VStack(spacing: 8) {
+                            Image(systemName: "message.fill")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                Text("1542")
+                                    .foregroundColor(.white)
+                            }
+                        })
+                        Button(action: {}, label: {
+                            VStack {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                Text("Share")
+                                    .foregroundColor(.white)
+                            }
+                        })
+                    }
+                    .padding(.bottom, 55)
+                    .padding(.trailing)
+                }
+//                Bottom Menu
                 HStack(spacing: 0) {
-                    Button(action: {}, label: {
+                    Button(action: {
+                        self.index = 0
+                    }, label: {
                         Image("home")
                             .resizable()
                             .frame(width: 25, height: 25)
@@ -37,7 +98,9 @@ struct Home: View {
                     })
                     Spacer(minLength: 0)
                     
-                    Button(action: {}, label: {
+                    Button(action: {
+                        self.index = 1
+                    }, label: {
                         Image("search")
                             .resizable()
                             .frame(width: 25, height: 25)
@@ -55,7 +118,9 @@ struct Home: View {
                     })
                     Spacer(minLength: 0)
                     
-                    Button(action: {}, label: {
+                    Button(action: {
+                        self.index = 2
+                    }, label: {
                         Image("comment")
                             .resizable()
                             .frame(width: 25, height: 25)
@@ -64,8 +129,10 @@ struct Home: View {
                     })
                     Spacer(minLength: 0)
                     
-                    Button(action: {}, label: {
-                        Image("search")
+                    Button(action: {
+                        self.index = 3
+                    }, label: {
+                        Image("profile")
                             .resizable()
                             .frame(width: 25, height: 25)
                             .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.35))
@@ -73,7 +140,11 @@ struct Home: View {
                     })
                 }
                         .padding(.horizontal)
+                }
+            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+            .padding(.bottom, (UIApplication.shared.windows.first?.safeAreaInsets.bottom)! + 5)
             }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
+        .edgesIgnoringSafeArea(.all)
         }
-    }
 }
