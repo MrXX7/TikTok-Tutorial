54e9035fee8ec43386da8d1b394908557fe6dbdd
Oncu <oohancan@gmail.com>
2022-05-08 02:12:55 +0300

    add func and class coordinator, add func updateUIview

--- View/PlayerScrollView.swift
+++ View/PlayerScrollView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct PlayerScrollView: UIViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        return PlayerScrollView.Coordinator(view: self)
+    }
+    
     
     @Binding var data: [Video]
     
@@ -26,8 +30,27 @@ struct PlayerScrollView: UIViewRepresentable {
         
         view.contentInsetAdjustmentBehavior = .never
         view.isPagingEnabled = true
+        view.delegate = context.coordinator
+        
         return view
     }
     func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(data.count))
+        
+        for i in 0..<uiView.subviews.count {
+            uiView.subviews[i].frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * CGFloat(data.count))
+        }
+    }
+
+    class Coordinator: NSObject, UIScrollViewDelegate {
+        var view: PlayerScrollView
+        
+        init(view: PlayerScrollView) {
+            self.view = view
+        }
+        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+            let index = Int(scrollView.contentOffset.y / UIScreen.main.bounds.height)
+            print(index)
+        }
     }
 }
