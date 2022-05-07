325e3c079ada4fe32fea983fdbdba302a654667e
Oncu <oohancan@gmail.com>
2022-05-07 07:19:43 +0300

    add player struct and home view

---
+++ View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TikTok Tutorial
+//
+//  Created by Oncu Ohancan on 5.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ View/Player.swift
@@ -0,0 +1,41 @@
+//
+//  Player.swift
+//  TikTok Tutorial
+//
+//  Created by Oncu Ohancan on 7.05.2022.
+//
+
+import SwiftUI
+import AVKit
+
+struct PlayerView: View {
+
+    @Binding var data: [Video]
+
+var body: some View {
+    VStack(spacing: 0) {
+        ForEach(0 ..< self.data.count) { index in
+            ZStack {
+                Player(player: data[index].player)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .offset(y: -5)
+                    }
+                }
+            }
+        }
+    }
+
+struct Player: UIViewControllerRepresentable {
+    
+    var player: AVPlayer
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let view = AVPlayerViewController()
+        view.player = player
+        view.showsPlaybackControls = false
+        view.videoGravity = .resizeAspectFill
+        return view
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+         
+    }
+}
